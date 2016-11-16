aba136593a8238758ec9cac8de22fee7a86f8799
Andrew Richardson <andyandy698@gmail.com>
2016-11-16 02:58:03 -0600

    Calculation for GPA; Formatting fixes Finalized the calculation for the GPA and fixed a few formatting issues that resulted from changing the algorithm used to insert new cells into the table element.

--- res/calculator.js
+++ res/calculator.js
@@ -1,15 +1,36 @@
 var numberOfClasses = 1;
 
 function addClasses() {
-    var html = document.getElementById('table-e').getElementsByTagName('tbody')[0].innerHTML;
-    var template = document.getElementById("template").getElementsByTagName('table')[0].getElementsByTagName('tbody')[0].innerHTML;
-    numberOfClasses = parseInt(document.getElementById("numClasses").value);
-    for(i = 0; i < numberOfClasses-1; i++){
+    var html = "";
+    var template = document.getElementById('template').getElementsByTagName('table')[0].getElementsByTagName('tbody')[0].innerHTML;
+    var template2 = document.getElementById('template2').getElementsByTagName('table')[0].getElementsByTagName('tbody')[0].innerHTML;
+    numberOfClasses = parseInt(document.getElementById('numClasses').value);
+    html += template2;
+    for(var i = 0; i < numberOfClasses; i++){
         html += template;
     }
-    document.getElementById('table-e').getElementsByTagName('tbody')[0].innerHTML = html;
+    document.getElementById('table-content').innerHTML = "";
+    document.getElementById('table-content').innerHTML = html;
 }
 
 function calculateGPA(){
-
+    var gpa = 0.0;
+    var totalGPA = 0.0;
+    var totalHours = 0;
+    var currentHours = 0;
+    var currentGPA = 0.0;
+    for(i = 0; i < numberOfClasses; i++){
+        currentGPA =  4-parseInt(document.getElementById('table-e').getElementsByTagName('tbody')[0].getElementsByTagName('select')[i*2].selectedIndex);
+        currentHours = parseInt(document.getElementById('table-e').getElementsByTagName('tbody')[0].getElementsByTagName('select')[i*2+1].value);
+        totalHours += currentHours;
+        totalGPA += currentHours * currentGPA;
+    }
+    gpa = totalGPA / totalHours;
+    var s_gpa = (Math.round(gpa*100)/100) + "";
+    if(s_gpa.length < 2){
+        s_gpa += ".00";
+    }else if(s_gpa.length < 4){
+        s_gpa += "0"
+    }
+    document.getElementById('gpa-result').innerText = "GPA: " + s_gpa;
 }
